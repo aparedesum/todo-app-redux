598b23de1f914f0c4ff5d3c852b5f41ee9076a93
Alfonso Paredes <alfonsomanuel.paredes@fisglobal.com>
2019-01-29 17:02:54 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,89 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const expect = require('expect.js');
+const deepFreeze = require('deep-freeze');
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return { id: action.id, text: action.text, completed: false };
+    case 'TOGGLE_TODO':
+        if (action.id === state.id) {
+          return { ...state, completed: !state.completed };
+        }
+        return state;
+    default: return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, todo(undefined, action)];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default: return state;
+  }
+};
+
+const toggleTodo = (todo) => {
+  return Object.assign({}, todo, { completed: !todo.completed });
+  //return {...todo, completed: !todo.completed}
+};
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: "ADD_TODO",
+    id: 0,
+    text: "Learn Redux"
+  };
+
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(stateAfter);
+
+  expect(todos(stateBefore, action)).to.eql(stateAfter);
+
+};
+
+const testToggleTodo = () => {
+  const stateBefore = [
+    { id: 0, text: "Learn Redux", completed: false },
+    { id: 1, text: "Go Shopping", completed: false }
+
+  ];
+
+  const action = {
+    type: "TOGGLE_TODO",
+    id: 1
+  };
+
+  const stateAfter = [
+    { id: 0, text: "Learn Redux", completed: false },
+    { id: 1, text: "Go Shopping", completed: true }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).to.eql(stateAfter);
+
+};
+
+
+it('test toggletodo before eq after', () => {
+  testToggleTodo();
+});
+
+it('test add toggle_todo before eq after', () => {
+  testAddTodo();
 });
